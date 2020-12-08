c975a81145b2fa04b5661e2c9637a2e3b801d1f5
shivam232323 <66739415+shivam232323@users.noreply.github.com>
2020-12-08 20:02:33 +0530

    updated database 2

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const data =knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl:true,
+    ssl: {
+      rejectUnauthorized: false
+    }
    
   }
 });

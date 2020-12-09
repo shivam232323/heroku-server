82bf89722dd294bbcf9c3fecb8cd3f04b4a01e13
shivam232323 <66739415+shivam232323@users.noreply.github.com>
2020-12-09 20:18:44 +0530

    n

---
+++ server copy.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+var cors = require('cors');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var knex = require('knex');
+
+
+
+const data =knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+   
+  }
+});
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+app.post('/register',(req ,res) => {
+  const { id ,name ,email ,pass} = req.body;
+  
+  const hash = bcrypt.hashSync(pass, saltRounds);
+
+  data.transaction(trx => {
+
+    trx.insert({
+      hash:hash,
+      email:email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+      .returning('*')
+      .insert({
+        name:name,
+        email:loginEmail[0],
+        joined: new Date()
+      })
+      .then( user => {
+        res.json(user[0]);
+      })
+    })
+  .then(trx.commit)
+  .catch(trx.rollback)
+  })
+  .catch(error => res.json('UNABLE TO REGIStER!'));
+
+});
+
+
+app.post('/signin',(req,res) => {
+  let count =0;
+  data.select('email', 'hash').from('login')
+  .where('email', '=', req.body.email)
+  .then(
+    value => {
+      
+      bcrypt.compare(req.body.password, value[0].hash, function(err, result) {
+        
+        if(result === true && req.body.password.length > 1)
+        {
+         
+           res.json('pass');
+           
+        }
+
+       
+        
+    })
+     
+  })
+  .catch(err => res.json('not Found'))
+});
+
+
+app.listen(process.env.PORT || 8080 ,() => {
+  console.log(`app is running on ${process.env.PORT}`)});

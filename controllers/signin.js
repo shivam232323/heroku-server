16187b1b8ab792671498808bccbebf8110c1fbfe
shivam232323 <66739415+shivam232323@users.noreply.github.com>
2020-12-09 22:03:11 +0530

    new

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,15 +1,16 @@
-const handleSignin = (data, bcrypt) => (req, res) => {
+const handleSignin = (db, bcrypt) => (req, res) => {
     const { email, password } = req.body;
-    res.json(email,password);
+    
     if (!email || !password) {
+        res.json(email ,password);
       return res.status(400).json('incorrect form submission');
     }
-    data.select('email', 'hash').from('login')
+    db.select('email', 'hash').from('login')
       .where('email', '=', email)
       .then(data => {
         const isValid = bcrypt.compareSync(password, data[0].hash);
         if (isValid) {
-          return data.select('*').from('users')
+          return db.select('*').from('users')
             .where('email', '=', email)
             .then(user => {
               res.json("pass")
